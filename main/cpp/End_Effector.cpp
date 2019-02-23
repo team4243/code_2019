7dababfcf555f6c93a392c80cd3af79ab4f83282
Team 4243 <team4243@gmail.com>
2019-02-23 14:54:03 -0500

    Updated Code
    Lots of cleanup, untested

--- main/cpp/End_Effector.cpp
+++ main/cpp/End_Effector.cpp
@@ -13,14 +13,20 @@
 /*************************************************************************************************/
 /**** Definitions ****/
 
+// Enable Cargo Limit Switch
+#define ENABLE_LIMIT_SWITCH_CARGO (false)
+
 // Rollers CAN device numbers
 #define CARGO_ROLLER_DEVICENUMBER_LEADER (24)
 #define CARGO_ROLLER_DEVICENUMBER_FOLLOWER (25)
 
-// Hatch Flower Servo PWM Channel
+// Hatch Flower Servo -- PWM Channel
 #define HATCH_FLOWER_PWM_CHANNEL (1)
 
-// End Effector Limit Switch Channel
+// Camera Tilt Servo -- PWM Channel
+#define CAMERA_TILT_PWM_CHANNEL (2)
+
+// End Effector Limit Switch -- DigitalIO Channel
 #define LIMIT_SWITCH_CHANNEL (2)
 
 // Speed as Rotations Per Second (RPS)
@@ -33,8 +39,16 @@
 #define ROLLERS_PEAK_OUTPUT_FWD (0.35)
 #define ROLLERS_PEAK_OUTPUT_REV (-0.35)
 
+// Hatch Flower MAX/MIN values
+#define HATCH_FLOWER_MAX (0.8)
+#define HATCH_FLOWER_MIN (0.25)
+
+// Camera Tilt MAX/MIN values
+#define CAMERA_TILT_MAX (0.7)
+#define CAMERA_TILT_MIN (0.2)
+
 /*************************************************************************************************/
-/**** Declarations ****/
+/**** Object Declarations and Global Variables ****/
 
 // Cargo Motor Drivers
 WPI_TalonSRX Cargo_Roller_Leader{CARGO_ROLLER_DEVICENUMBER_LEADER};
@@ -43,9 +57,11 @@ WPI_TalonSRX Cargo_Roller_Follower{CARGO_ROLLER_DEVICENUMBER_FOLLOWER};
 // End Effector -- Hatch Catch Servo
 frc::PWM Hatch_Flower_Servo(HATCH_FLOWER_PWM_CHANNEL);
 
-//We are adding a limit switch to check to see if the cargo is firmly secured within the end effector.
-//pressed limit switch = true
-// DigitalInput End_Effector_Limit_Switch(LIMIT_SWITCH_CHANNEL);
+// End Effector -- Camera Tilt Servo
+frc::PWM Camera_Tilt_Servo(CAMERA_TILT_PWM_CHANNEL);
+
+//We are adding a limit switch to check to see if the cargo is firmly secured within the end effector
+frc::DigitalInput End_Effector_Limit_Switch(LIMIT_SWITCH_CHANNEL);
 
 /*************************************************************************************************/
 /**** Configuration ****/
@@ -61,45 +77,34 @@ void Excelsior_End_Effector::Configure_End_Effector()
 /*************************************************************************************************/
 /**** Actions ****/
 
-void Excelsior_End_Effector::Cargo_Roller_Action(bool dispense, double speed, bool manual)
+void Excelsior_End_Effector::Cargo_Roller_Action(bool dispense, double speed)
 {
-    // Auto
-    if (!manual)
-    {
-        if (dispense)
-        {
-            Cargo_Roller_Leader.Set(ControlMode::Velocity, speed * CARGO_ROLLER_SPEED_RPS * CONVERT_TO_RPS_EE);
-        }
+    if (dispense)
+        Cargo_Roller_Leader.Set(ControlMode::Velocity, speed * CARGO_ROLLER_SPEED_RPS * CONVERT_TO_RPS_EE);
 
-        else
-        {
-            // if(!End_Effector_Limit_Switch.Get())
-            Cargo_Roller_Leader.Set(ControlMode::Velocity, -speed * CARGO_ROLLER_SPEED_RPS * CONVERT_TO_RPS_EE);
-        }
-    }
+    else if (!ENABLE_LIMIT_SWITCH_CARGO || !End_Effector_Limit_Switch.Get())
+        Cargo_Roller_Leader.Set(ControlMode::Velocity, -speed * CARGO_ROLLER_SPEED_RPS * CONVERT_TO_RPS_EE);
+}
 
-    // Manual
-    else
-    {
-        if (dispense)
-        {
-            Cargo_Roller_Leader.Set(ControlMode::PercentOutput, speed);
-        }
-
-        else
-        {
-            // if(!End_Effector_Limit_Switch.Get())
-            Cargo_Roller_Leader.Set(ControlMode::PercentOutput, -speed);
-        }
-    }
+void Excelsior_End_Effector::Cargo_Roller_Manual(double speed)
+{
+    Cargo_Roller_Leader.Set(ControlMode::PercentOutput, speed);
 }
 
 void Excelsior_End_Effector::Hatch_Flower_Action(bool extend)
 {
     if (extend)
-        Hatch_Flower_Servo.SetPosition(1);
+        Hatch_Flower_Servo.SetPosition(HATCH_FLOWER_MAX);
     else
-        Hatch_Flower_Servo.SetPosition(0);
+        Hatch_Flower_Servo.SetPosition(HATCH_FLOWER_MIN);
+}
+
+void Excelsior_End_Effector::Camera_Tilt_Action(bool tiltUp)
+{
+    if (tiltUp)
+        Camera_Tilt_Servo.SetPosition(CAMERA_TILT_MAX);
+    else
+        Camera_Tilt_Servo.SetPosition(CAMERA_TILT_MIN);
 }
 
 /*************************************************************************************************/
--- main/cpp/Robot.cpp
+++ main/cpp/Robot.cpp
@@ -15,60 +15,70 @@
 #define ENABLE_PAYLOAD_LIFT (false)
 #define ENABLE_END_EFFECTOR (true)
 
-// Enabled Encoder Printing
+// Print encoder values for any ENABLED mechanisms
 #define PRINT_ENCODER_VALUES (true)
-
-// Joystick COM channels
-#define DRIVER_ONE_CHANNEL (0)
-#define DRIVER_TWO_CHANNEL (1)
 
 // Deadband for Triggers
 #define DEADBAND_TRIGGER (0.12)
 
 /*************************************************************************************************/
-/**** Control Logic Switchboard Definitions ****/
+/**** Control Logic Switchboard Definitions -- Omni Drive ****/
 
-// Drive logic
+// Drive logic -- These are pass directly into the Omni Drive Action
 #define CTRL_DRIVE_LEFT_RIGHT (Driver_One.GetX())
 #define CTRL_DRIVE_FWD_BWD (-Driver_One.GetY())
 #define CTRL_DRIVE_ROTATE (Driver_One.GetRawAxis(AXIS_RIGHT_X))
+#define CTRL_DRIVE_SWITCH_MANUAL (Driver_One.GetRawButton(BUTTON_BUMPER_LEFT) && Driver_One.GetRawButton(BUTTON_BUMPER_RIGHT))
 
-#define CTRL_DRIVE_SWITCH_MANUAL (Driver_One.GetRawButton(BUTTON_BUMPER_RIGHT))
+/*************************************************************************************************/
+/**** Control Logic Switchboard Definitions -- Payload Lift ****/
 
-// Lift logic -- Position Mode
-#define CTRL_LIFT_POSITION_LOW_CARGO (Driver_One.GetRawButton(BUTTON_GREEN) && Driver_One.GetRawButton(BUTTON_BUMPER_LEFT))
-#define CTRL_LIFT_POSITION_LOW_HATCH (Driver_One.GetRawButton(BUTTON_GREEN))
-#define CTRL_LIFT_POSITION_MID_CARGO (Driver_One.GetRawButton(BUTTON_RED) && Driver_One.GetRawButton(BUTTON_BUMPER_LEFT))
-#define CTRL_LIFT_POSITION_MID_HATCH (Driver_One.GetRawButton(BUTTON_RED))
-#define CTRL_LIFT_POSITION_HIGH_CARGO (Driver_One.GetRawButton(BUTTON_YELLOW) && Driver_One.GetRawButton(BUTTON_BUMPER_LEFT))
-#define CTRL_LIFT_POSITION_HIGH_HATCH (Driver_One.GetRawButton(BUTTON_YELLOW))
+// Lift logic -- Position Mode -- Cargo
+#define CTRL_LIFT_POSITION_LOW_CARGO (Driver_Two.GetRawButton(BUTTON_GREEN) && Driver_Two.GetRawButton(BUTTON_BUMPER_LEFT))
+#define CTRL_LIFT_POSITION_MID_CARGO (Driver_Two.GetRawButton(BUTTON_RED) && Driver_Two.GetRawButton(BUTTON_BUMPER_LEFT))
+#define CTRL_LIFT_POSITION_HIGH_CARGO (Driver_Two.GetRawButton(BUTTON_YELLOW) && Driver_Two.GetRawButton(BUTTON_BUMPER_LEFT))
+
+// Lift logic -- Position Mode -- Hatch
+#define CTRL_LIFT_POSITION_LOW_HATCH (Driver_Two.GetRawButton(BUTTON_GREEN) && Driver_Two.GetRawButton(BUTTON_BUMPER_RIGHT))
+#define CTRL_LIFT_POSITION_MID_HATCH (Driver_Two.GetRawButton(BUTTON_RED) && Driver_Two.GetRawButton(BUTTON_BUMPER_RIGHT))
+#define CTRL_LIFT_POSITION_HIGH_HATCH (Driver_Two.GetRawButton(BUTTON_YELLOW) && Driver_Two.GetRawButton(BUTTON_BUMPER_RIGHT))
 
 // Lift logic -- Position Adjustment
 #define CTRL_LIFT_POSITION_STEP_UP (Driver_One.GetPOV() == 0)
 #define CTRL_LIFT_POSITION_STEP_DOWN (Driver_One.GetPOV() == 180)
 
 // Lift logic -- Manual
-#define CTRL_LIFT_UP ()
-#define CTRL_LIFT_DOWN ()
-#define CTRL_LIFT_STOP ()
-
-// Cargo Roller logic -- Velocity Mode
-#define CTRL_ROLL_IN (Driver_One.GetRawAxis(AXIS_LEFT_TRIGGER))
-#define CTRL_ROLL_OUT (Driver_One.GetRawAxis(AXIS_RIGHT_TRIGGER))
-
-#define CTRL_SWITCH_TO_ROLLERS (Driver_One.GetRawButton(BUTTON_BUMPER_LEFT))
-#define CTRL_ROLL_MANUAL_SWITCH (Driver_One.GetRawButton(BUTTON_BUMPER_RIGHT))
-
-// Hatch logic
-#define CTRL_HATCH_IN (Driver_One.GetRawAxis(AXIS_LEFT_TRIGGER))
-#define CTRL_HATCH_OUT (Driver_One.GetRawAxis(AXIS_RIGHT_TRIGGER))
-
-// Camera Tilt logic
-#define CTRL_CAMERA_UP ()
-#define CTRL_CAMERA_DOWN ()
+#define CTRL_LIFT_UP_DOWN (-Driver_One.GetY())
+#define CTRL_LIFT_SWITCH_MANUAL (Driver_One.GetRawButton(BUTTON_RIGHT_STICK_PRESS))
 
 // Lift Encoder Zero logic
-#define CTRL_LIFT_ENCODER_ZERO (Driver_One.GetRawButton(BUTTON_BACK) && Driver_One.GetRawButton(BUTTON_START))
+#define CTRL_ALL_BUMPERS (Driver_One.GetRawButton(BUTTON_BUMPER_LEFT) && Driver_One.GetRawButton(BUTTON_BUMPER_RIGHT))
+#define CTRL_ALL_TRIGGERS (Driver_One.GetRawAxis(AXIS_LEFT_TRIGGER) == 1 && Driver_One.GetRawAxis(AXIS_RIGHT_TRIGGER) == 1)
+#define CTRL_ALL_MENUS (Driver_One.GetRawButton(BUTTON_BACK) && Driver_One.GetRawButton(BUTTON_START))
+#define CTRL_LIFT_ENCODER_ZERO (CTRL_ALL_BUMPERS && CTRL_ALL_TRIGGERS && CTRL_ALL_MENUS)
+
+/*************************************************************************************************/
+/**** Control Logic Switchboard Definitions -- End Effector ****/
+
+// Trigger logic for Cargo Roller and Hatch Flower
+#define LEFT_TRIGGER_VALUE (Driver_One.GetRawAxis(AXIS_LEFT_TRIGGER))
+#define RIGHT_TRIGGER_VALUE (Driver_One.GetRawAxis(AXIS_RIGHT_TRIGGER))
+
+// Cargo Roller logic -- Velocity Mode
+#define CTRL_ROLL_IN (LEFT_TRIGGER_VALUE > DEADBAND_TRIGGER && Driver_Two.GetRawButton(BUTTON_BUMPER_LEFT))
+#define CTRL_ROLL_OUT (RIGHT_TRIGGER_VALUE > DEADBAND_TRIGGER && Driver_Two.GetRawButton(BUTTON_BUMPER_LEFT))
+
+// Cargo Roller logic -- MANUAL
+#define CTRL_ROLL_IN_OUT (-Driver_Two.GetY())
+#define CTRL_ROLL_SWITCH_MANUAL (Driver_Two.GetRawButton(BUTTON_RIGHT_STICK_PRESS))
+
+// Hatch Flower logic
+#define CTRL_HATCH_IN (LEFT_TRIGGER_VALUE > DEADBAND_TRIGGER && Driver_Two.GetRawButton(BUTTON_BUMPER_RIGHT))
+#define CTRL_HATCH_OUT (RIGHT_TRIGGER_VALUE > DEADBAND_TRIGGER && Driver_Two.GetRawButton(BUTTON_BUMPER_RIGHT))
+
+// Camera Tilt logic
+#define CTRL_CAMERA_UP (Driver_One.GetRawAxis(AXIS_LEFT_TRIGGER) == 1 || Driver_Two.GetRawButton(BUTTON_YELLOW))
+#define CTRL_CAMERA_DOWN (Driver_One.GetRawAxis(AXIS_RIGHT_TRIGGER) == 1 || Driver_Two.GetRawButton(BUTTON_GREEN))
 
 /*************************************************************************************************/
 /**** Object Declarations and Global Variables ****/
@@ -82,11 +92,11 @@ Excelsior_End_Effector End_Effector;
 frc::Joystick Driver_One{DRIVER_ONE_CHANNEL};
 frc::Joystick Driver_Two{DRIVER_TWO_CHANNEL};
 
-// Keeping track of the current Lift position
-int targetPayloadHeight = -1;
-
 // Operator must press and release POV for Payload Lift position changes
 bool pressedLastFrame_autoLift = false;
+
+// Operator must release manual lift driving button to stop motion
+bool pressedLastFrame_manualLift = false;
 
 /*************************************************************************************************/
 /**** Teleop Periodic ****/
@@ -109,76 +119,58 @@ void Robot::TeleopPeriodic()
     {
         // Zero out the Lift Arm
         if (CTRL_LIFT_ENCODER_ZERO)
-            Payload_Lift.Configure_Payload_Lift();
+            Payload_Lift.Zero_Encoder_Position();
 
-        // Cargo LOW
-        if (CTRL_LIFT_POSITION_LOW_CARGO)
-        {
-            targetPayloadHeight = (int)Lowest_Cargo_Position;
+        // Set Lift Position -- Cargo LOW
+        else if (CTRL_LIFT_POSITION_LOW_CARGO)
             Payload_Lift.Payload_Lift_Action(Lowest_Cargo_Position);
-        }
 
-        // Hatch LOW
+        // Set Lift Position -- Hatch LOW
         else if (CTRL_LIFT_POSITION_LOW_HATCH)
-        {
-            targetPayloadHeight = (int)Lowest_Hatch_Position;
             Payload_Lift.Payload_Lift_Action(Lowest_Hatch_Position);
-        }
 
-        // Cargo MIDDLE
+        // Set Lift Position -- Cargo MIDDLE
         else if (CTRL_LIFT_POSITION_MID_CARGO)
-        {
-            targetPayloadHeight = (int)Middle_Cargo_Position;
             Payload_Lift.Payload_Lift_Action(Middle_Cargo_Position);
-        }
 
-        // Hatch MIDDLE
+        // Set Lift Position -- Hatch MIDDLE
         else if (CTRL_LIFT_POSITION_MID_HATCH)
-        {
-            targetPayloadHeight = (int)Middle_Hatch_Position;
             Payload_Lift.Payload_Lift_Action(Middle_Hatch_Position);
-        }
 
-        // Cargo HIGH
+        // Set Lift Position -- Cargo HIGH
         else if (CTRL_LIFT_POSITION_HIGH_CARGO)
-        {
-            targetPayloadHeight = (int)Highest_Cargo_Position;
             Payload_Lift.Payload_Lift_Action(Highest_Cargo_Position);
-        }
 
-        // Hatch HIGH
+        // Set Lift Position -- Hatch HIGH
         else if (CTRL_LIFT_POSITION_HIGH_HATCH)
-        {
-            targetPayloadHeight = (int)Highest_Hatch_Position;
             Payload_Lift.Payload_Lift_Action(Highest_Hatch_Position);
-        }
 
-        // Move to next highest lift position
+        // Set Lift Position -- Move to next highest lift position
         else if (CTRL_LIFT_POSITION_STEP_UP)
         {
+            // Intentionally separated, don't combine with CTRL_LIFT_POSITION_STEP_UP conditional
             if (!pressedLastFrame_autoLift)
             {
                 pressedLastFrame_autoLift = true;
-                if (targetPayloadHeight < Maximum_Height_Position)
-                {
-                    targetPayloadHeight++;
-                }
-                Payload_Lift.Payload_Lift_Action((Payload_Lift_Position)targetPayloadHeight);
+                Payload_Lift.Payload_Lift_Step(true);
             }
         }
 
-        // Move to next lowest lift position
+        // Set Lift Position -- Move to next lowest lift position
         else if (CTRL_LIFT_POSITION_STEP_DOWN)
         {
             if (!pressedLastFrame_autoLift)
             {
                 pressedLastFrame_autoLift = true;
-                if (targetPayloadHeight > Ground_Position)
-                {
-                    targetPayloadHeight--;
-                }
-                Payload_Lift.Payload_Lift_Action((Payload_Lift_Position)targetPayloadHeight);
+                Payload_Lift.Payload_Lift_Step(false);
             }
+        }
+
+        // Set Lift Position -- MANUAL
+        else if (CTRL_LIFT_SWITCH_MANUAL)
+        {
+            Payload_Lift.Payload_Lift_Manual(CTRL_LIFT_UP_DOWN);
+            pressedLastFrame_manualLift = true;
         }
 
         // To prevent multiple presses of lift position adjustment
@@ -187,41 +179,44 @@ void Robot::TeleopPeriodic()
             pressedLastFrame_autoLift = false;
         }
 
+        // Stop the Lift if operator releases manual switch
+        if (!CTRL_LIFT_SWITCH_MANUAL && pressedLastFrame_manualLift)
+        {
+            Payload_Lift.Payload_Lift_Manual(0);
+            pressedLastFrame_manualLift = false;
+        }
+
         // Print raw encoder values
         if (PRINT_ENCODER_VALUES)
-            Payload_Lift.Print_Lift_Encoder((Payload_Lift_Position)targetPayloadHeight);
+            Payload_Lift.Print_Lift_Encoder();
     }
 
     /************* END EFFECTOR LOGIC ***************/
     if (ENABLE_END_EFFECTOR)
     {
-        double Left_Trigger_Value = CTRL_ROLL_OUT;
-        double Right_Trigger_Value = CTRL_ROLL_IN;
-
-        bool Left_Trigger_Pressed = (Left_Trigger_Value > DEADBAND_TRIGGER);
-        bool Right_Trigger_Pressed = (Right_Trigger_Value > DEADBAND_TRIGGER);
-
         // Cargo Rollers
-        if (Left_Trigger_Pressed && CTRL_SWITCH_TO_ROLLERS)
-        {
-            End_Effector.Cargo_Roller_Action(true, Left_Trigger_Value, CTRL_ROLL_MANUAL_SWITCH);
-        }
+        if (CTRL_ROLL_SWITCH_MANUAL)
+            End_Effector.Cargo_Roller_Manual(CTRL_ROLL_IN_OUT);
 
-        else if (Right_Trigger_Pressed && CTRL_SWITCH_TO_ROLLERS)
-        {
-            End_Effector.Cargo_Roller_Action(false, Right_Trigger_Value, CTRL_ROLL_MANUAL_SWITCH);
-        }
+        else if (CTRL_ROLL_IN)
+            End_Effector.Cargo_Roller_Action(true, LEFT_TRIGGER_VALUE);
+
+        else if (CTRL_ROLL_OUT)
+            End_Effector.Cargo_Roller_Action(false, RIGHT_TRIGGER_VALUE);
 
         // Hatch Flower
-        else if (Left_Trigger_Pressed)
-        {
+        else if (CTRL_HATCH_IN)
             End_Effector.Hatch_Flower_Action(true);
-        }
 
-        else if (Right_Trigger_Pressed)
-        {
+        else if (CTRL_HATCH_OUT)
             End_Effector.Hatch_Flower_Action(false);
-        }
+
+        // Camera Tilt
+        else if (CTRL_CAMERA_UP)
+            End_Effector.Camera_Tilt_Action(true);
+
+        else if (CTRL_CAMERA_DOWN)
+            End_Effector.Camera_Tilt_Action(false);
 
         // Print raw encoder values
         if (PRINT_ENCODER_VALUES)
